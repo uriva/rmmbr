8437fe454f7a1b983124bb6a7e12c642f2918dd8
Eyal Greenberg <eyalgr99@gmail.com>
2023-05-06 01:53:05 +0300

    refactor the web framework

--- server/src/webFramework.ts
+++ server/src/webFramework.ts
@@ -9,55 +9,49 @@ interface Router {
   [index: string]: MethodHandler;
 }
 
-export const app = (router: Router): Handler => {
-  const routes: Array<[RegExp, MethodHandler]> = Object.entries(router).map(
-    ([url_pattern, handler]) => [new RegExp(`^${url_pattern}$`), handler]
-  );
-
-  return (
-    request: Request,
-    connInfo: ConnInfo
-  ): Response | Promise<Response> => {
-    for (const route of routes) {
-      const [pattern, methodHandler] = route;
-      const pathname = new URL(request.url).pathname;
-      const myMatch = pathname.match(pattern);
-      if (myMatch == undefined) {
+const appHandler =
+  (routes: Array<[RegExp, MethodHandler]>) =>
+  (request: Request, connInfo: ConnInfo): Response | Promise<Response> => {
+    for (const [pattern, methodHandler] of routes) {
+      if (!new URL(request.url).pathname.match(pattern)) {
         continue;
       }
       const requestHandler =
         methodHandler[request.method as keyof MethodHandler];
-      if (requestHandler == undefined) {
+      if (!requestHandler) {
         continue;
       }
       return requestHandler(request, connInfo);
     }
     return Response404();
   };
-};
 
-export const Response401 = () =>
-  new Response("Authentication needed", { status: 401 });
+export const app = (router: Router): Handler =>
+  appHandler(
+    Object.entries(router).map(([urlPattern, handler]) => [
+      new RegExp(`^${urlPattern}$`),
+      handler,
+    ]),
+  );
 
 export const Response404 = () => new Response("Not Found", { status: 404 });
 
 type RequestAuthenticator<Auth> = (
-  request: Request
-) => Auth | null | Promise<Auth> | Promise<null>;
+  request: Request,
+) => Auth | null | Promise<Auth | null>;
 
 export type AuthenticatedHandler<Auth> = (
   request: Request,
   auth: NonNullable<Awaited<Auth>>,
-  connInfo: ConnInfo
+  connInfo: ConnInfo,
 ) => Response | Promise<Response>;
 
 export const authenticated =
   <T>(authenticator: RequestAuthenticator<T>) =>
   (handler: AuthenticatedHandler<T>): Handler =>
-  async (request, conn_info) => {
+  async (request, connInfo) => {
     const auth = await authenticator(request);
-    if (auth == null) {
-      return Response401();
-    }
-    return await handler(request, auth, conn_info);
+    return auth
+      ? await handler(request, auth, connInfo)
+      : new Response("Authentication needed", { status: 401 });
   };
