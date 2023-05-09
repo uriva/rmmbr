9e119e66112c796853014a81cf8a24fd1d9f91b0
uri <uri.valevski@gmail.com>
2023-05-09 15:04:00 +0200

    deploy-pypi

--- python/setup.py
+++ python/setup.py
@@ -7,7 +7,7 @@ _repo_dir = os.environ.get("GITHUB_WORKSPACE")
 assert _repo_dir
 setup(
     name="rmmbr",
-    version="0.0.3",
+    version="0.0.4",
     description="Simple persistent caching.",
     long_description=(pathlib.Path(_repo_dir) / "README.md").read_text(),
     long_description_content_type="text/markdown",
@@ -22,9 +22,7 @@ setup(
         "aiofiles",
         "pytest-asyncio",
     ],
-    entry_points={
-        "console_scripts": ["cache=cache.cli:main", "rmmbr-cli=rmmbr.cli:main"]
-    },
+    entry_points={"console_scripts": ["cache=cache.cli:main"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
