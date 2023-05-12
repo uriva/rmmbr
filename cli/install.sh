506352d0d48c9b98ba2d5eb3c6f4a35b055423e8
uri <uri.valevski@gmail.com>
2023-05-12 13:46:04 +0200

    improve message

--- cli/install.sh
+++ cli/install.sh
@@ -6,4 +6,4 @@ cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
 echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; npx deno-bin run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >rmmbr
 chmod +x rmmbr
 sudo mv rmmbr /usr/local/bin/
-echo "rmmbr cli installed. Restart your terminal and run \`rmmbr login\`"
+echo "rmmbr cli installed. Run \`rmmbr login\`"
