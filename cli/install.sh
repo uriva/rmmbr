f216d26a6f4582a0ad7fa531f6c8483ba1696727
Eyal Greenberg <eyalgr99@gmail.com>
2023-05-12 16:23:26 +0300

    download rmmbr cli to tmp file

--- cli/install.sh
+++ cli/install.sh
@@ -3,7 +3,8 @@ if ! command -v npx &>/dev/null; then
   exit 1
 fi
 cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
-echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; npx deno-bin run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >rmmbr
-chmod +x rmmbr
-sudo mv rmmbr /usr/local/bin/
+tmp_rmmbr=$(mktemp)
+echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; npx deno-bin run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >$tmp_rmmbr
+chmod +x $tmp_rmmbr
+sudo mv $tmp_rmmbr /usr/local/bin/rmmbr
 echo "rmmbr cli installed. Run \`rmmbr login\`"
