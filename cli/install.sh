95f98c662605c50ebda159e1299260128a5690e4
uri <uri.valevski@gmail.com>
2023-05-13 10:21:26 +0200

    move to deno

--- cli/install.sh
+++ cli/install.sh
@@ -1,10 +1,10 @@
-if ! command -v npx &>/dev/null; then
-  echo "npx is not installed. Please install npx and try again."
+if ! command -v deno &>/dev/null; then
+  echo "Deno is not installed. Please install it and try again."
   exit 1
 fi
 cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
 tmp_rmmbr=$(mktemp)
-echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; npx deno-bin run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >$tmp_rmmbr
+echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; deno run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >$tmp_rmmbr
 chmod +x $tmp_rmmbr
 sudo mv $tmp_rmmbr /usr/local/bin/rmmbr
 echo "rmmbr cli installed. Run \`rmmbr login\`"
