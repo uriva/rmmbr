e3ff82808ec7099e6ec8284edcff2d53c7fc87f9
uri <uri.valevski@gmail.com>
2023-05-13 10:30:13 +0200

    don't reload

--- cli/install.sh
+++ cli/install.sh
@@ -4,7 +4,7 @@ if ! command -v deno &>/dev/null; then
 fi
 cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
 tmp_rmmbr=$(mktemp)
-echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; deno run -r --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >$tmp_rmmbr
+echo "export RMMBR_SERVER=https://uriva-rmmbr.deno.dev; deno run --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"" >$tmp_rmmbr
 chmod +x $tmp_rmmbr
 sudo mv $tmp_rmmbr /usr/local/bin/rmmbr
 echo "rmmbr cli installed. Run \`rmmbr login\`"
