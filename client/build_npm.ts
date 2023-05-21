4c0296a27b5d23a4802864c96e80cfc46b29bf1c
Eyal Greenberg <eyalgr99@gmail.com>
2023-05-22 00:08:04 +0300

    deploy-npm

--- client/build_npm.ts
+++ client/build_npm.ts
@@ -11,7 +11,7 @@ await build({
   shims: { deno: true },
   package: {
     name: "rmmbr",
-    version: "0.0.12",
+    version: "0.0.13",
     description: "Easy caching.",
     license: "MIT",
     repository: {
