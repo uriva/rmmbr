5ae8e54e0ea25c446c0ef72fafdde7322a3b266b
uri <uri.valevski@gmail.com>
2023-06-22 10:17:43 +0300

    push-deno-recognition-to-end

--- cli/install.sh
+++ cli/install.sh
@@ -1,20 +1,21 @@
 set -e
+cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
+tmp_rmmbr=$(mktemp)
+echo "
+set -e
 if ! command -v deno &>/dev/null;
 then
   echo "Deno is required. Please install it and try again: https://deno.com/manual/getting_started/installation"
-else
-  cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
-  tmp_rmmbr=$(mktemp)
-  echo "
-  if [ \"\$1\" == \"update\" ] || [ \"\$1\" == \"upgrade\" ];
-  then
-    deno cache --reload $cli_location;
-  else
-    export RMMBR_SERVER=https://rmmbr.net;
-    deno run --allow-write --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"
-  fi
-  " >$tmp_rmmbr
-  chmod +x $tmp_rmmbr
-  mv $tmp_rmmbr /usr/local/bin/rmmbr
-  echo "rmmbr cli installed. Run \`rmmbr login\`"
+  exit
 fi
+if [ \"\$1\" == \"update\" ] || [ \"\$1\" == \"upgrade\" ];
+then
+  deno cache --reload $cli_location;
+else
+  export RMMBR_SERVER=https://rmmbr.net;
+  deno run --allow-write --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"
+fi
+" >$tmp_rmmbr
+chmod +x $tmp_rmmbr
+mv $tmp_rmmbr /usr/local/bin/rmmbr
+echo "rmmbr cli installed. Run \`rmmbr login\`"
