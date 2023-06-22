4633b5e54d20a68cde9db48b97f2bc5df107c719
uri <uri.valevski@gmail.com>
2023-06-22 09:39:41 +0300

    fix-install-script-bug

--- cli/install.sh
+++ cli/install.sh
@@ -1,13 +1,13 @@
 if ! command -v deno &>/dev/null; then
   echo "Deno is required. Please install it and try again: https://deno.com/manual/getting_started/installation"
-  exit 1
+  return 1
 fi
 cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
 tmp_rmmbr=$(mktemp)
 echo "
 if [ \"\$1\" == \"update\" ] || [ \"\$1\" == \"upgrade\" ]; then
   deno cache --reload $cli_location;
-  exit 0;
+  return 0;
 fi
 export RMMBR_SERVER=https://rmmbr.net;
 deno run --allow-write --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"
