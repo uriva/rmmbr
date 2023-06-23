b0a4fe1e93eae34a17273d54021bf486ad917a36
uri <uri.valevski@gmail.com>
2023-06-23 14:49:24 +0300

    deploy-cli

--- cli/install.sh
+++ cli/install.sh
@@ -1,21 +1,12 @@
-set -e
-cli_location=https://raw.githubusercontent.com/uriva/rmmbr/main/cli/src/index.ts
-script_desired_location=/usr/local/bin/rmmbr
-echo "
-set -e
-if ! command -v deno &>/dev/null;
-then
-  echo "Deno is required. Please install it and try again: https://deno.com/manual/getting_started/installation"
-  exit
-fi
-if [ \"\$1\" == \"update\" ] || [ \"\$1\" == \"upgrade\" ];
-then
-  deno cache --reload $cli_location;
-else
-  export RMMBR_SERVER=https://rmmbr.net;
-  deno run --allow-write --allow-run --allow-read --allow-net --allow-sys --allow-env $cli_location \"\$@\"
-fi
-" >$script_desired_location
-chown $SUDO_USER $script_desired_location
-chmod u+x $script_desired_location
+# set -e
+
+# Download the latest binary executable from GitHub.
+wget https://github.com/uriva/rmmbr/releases/download/latest/cli
+
+# Place the binary executable in the user's $HOME/bin directory.
+mv cli $HOME/bin/rmmbr
+
+# Make the binary executable executable.
+chmod +x $HOME/bin/rmmbr
+
 echo "rmmbr cli installed. Run \`rmmbr login\`"
