375a385a4ed362543db84598a6c0021416e8ee1e
uri <uri.valevski@gmail.com>
2023-06-29 11:01:57 +0300

    landing page initial commit

---
+++ landing-page/islands/Counter.tsx
@@ -0,0 +1,16 @@
+import type { Signal } from "@preact/signals";
+import { Button } from "../components/Button.tsx";
+
+interface CounterProps {
+  count: Signal<number>;
+}
+
+export default function Counter(props: CounterProps) {
+  return (
+    <div>
+      <p>{props.count}</p>
+      <Button onClick={() => props.count.value -= 1}>-1</Button>
+      <Button onClick={() => props.count.value += 1}>+1</Button>
+    </div>
+  );
+}
