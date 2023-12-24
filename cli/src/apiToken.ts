be8a51df401acdbb081135c87fac3f57b6f05a21
uri <uri.valevski@gmail.com>
2023-12-24 16:39:42 +0200

    bugfix

--- cli/src/apiToken.ts
+++ cli/src/apiToken.ts
@@ -4,10 +4,10 @@ const list = callServer("api-token/", "GET", undefined);
 const create = callServer("api-token/", "POST", { action: "create" });
 
 export const apiTokenCommands = {
-  create,
-  list,
+  create: () => create,
+  list: () => list,
   delete: (tokenId: string) =>
     callServer("api-token/", "POST", { action: "delete", tokenId }),
-  get: (secret: string) =>
+  get: () => (secret: string) =>
     list(secret).then((tokens: string[]) => tokens[0] || create(secret)),
 };
