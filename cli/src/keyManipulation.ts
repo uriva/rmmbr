612e6019671502e09b184eb529547d97e91ddad2
uri <uri.valevski@gmail.com>
2025-03-12 12:54:46 +0200

    bugfix

--- cli/src/keyManipulation.ts
+++ cli/src/keyManipulation.ts
@@ -1,5 +1,5 @@
+import { inputToCacheKey } from "../../client/src/crypto.ts";
 import { callServer } from "./rpc.ts";
-import { inputToCacheKey } from "../../client/src/index.ts";
 
 const actOnKey = (method: string) => (args: string) => () => {
   const [token, cacheId, jsonString] = args.split(" ");
