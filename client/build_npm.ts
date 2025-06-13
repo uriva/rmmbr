bf863a93ac37d9b52f2d557cdde593e055e55124
uri <uri.valevski@gmail.com>
2025-06-13 14:56:08 +0300

    deploy-npm

--- client/build_npm.ts
+++ client/build_npm.ts
@@ -12,7 +12,7 @@ await build({
   shims: { deno: true },
   package: {
     name: "rmmbr",
-    version: "0.0.24",
+    version: "0.0.25",
     description: "Easy caching.",
     license: "MIT",
     repository: {
